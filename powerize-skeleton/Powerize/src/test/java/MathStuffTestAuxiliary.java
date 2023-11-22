154276458aca7b763cba061711c1711cb5866fd4
Naomi-Han <64907009+Naomi-Han@users.noreply.github.com>
2023-11-22 09:38:34 +0100

    Extracted Files added

---
+++ powerize-skeleton/Powerize/src/test/java/MathStuffTestAuxiliary.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.util.*;
+
+/**
+ * Test cases for auxiliary methods in {@code MathStuff}.
+ *
+<!--//# BEGIN TODO: Name, student ID, and date-->
+<p><b>Replace this line</b></p>
+<!--//# END TODO-->
+ */
+public class MathStuffTestAuxiliary {
+
+//# BEGIN TODO: Test cases for auxiliary functions
+// Replace this line
+//# END TODO
+
+}
